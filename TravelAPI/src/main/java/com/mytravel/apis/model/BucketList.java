ea59889328b2cccb6232a23cba2da423f761e042
Binayak Nanda <binayak.nanda@gmail.com>
2020-08-22 20:44:52 +0530

    Initial Check in.

---
+++ TravelAPI/src/main/java/com/mytravel/apis/model/BucketList.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package com.mytravel.apis.model;
+
+/**
+ * @author HP
+ *
+ */
+public class BucketList {
+	private long id;
+	private String name;
+
+	public BucketList(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
